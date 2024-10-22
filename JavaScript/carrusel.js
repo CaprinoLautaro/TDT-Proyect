98f76c536648c06a0b0babf364771507452e06bf
HexenWin <ramirobruzon@gmail.com>
2024-10-22 02:55:55 -0300

    css y js
    en css le agrege un hoover que voy a ir trabajando , en js agrege un array de img y en la funcion que hice de carrousel, ahora linkea imagen[i] con linksImg[i] . hf !

--- JavaScript/carrusel.js
+++ JavaScript/carrusel.js
@@ -1,5 +1,9 @@
 let imagenes = ['img/cursos/curso python1.jpg','img/cursos/curso-java.webp','img/cursos/curso-html.webp','img/cursos/curso-css.webp','img/cursos/curso-php.webp']
-    let contador = 0;
+    
+
+let linksImg = ['pages/pagina-python.html','pages/pagina-java.html','pages/pagina-html.html','pages/pagina-css.html','pages/pagina-php.html'];
+
+let contador = 0;
 
 function carrousel(contenedor){
             contenedor.addEventListener('click', e => {
@@ -7,31 +11,32 @@ function carrousel(contenedor){
                     adelante = contenedor.querySelector('.boton-adelante'),
                     img = contenedor.querySelector('img'),
                     tgt = e.target;
-
-                    if(tgt == atras){
-                        if(contador > 0){
-                            img.src = imagenes[contador - 1];
-                            contador--;
-                        }else{
-                            img.src = imagenes[imagenes.length - 1];
-                            contador = imagenes.length -1;
+    if(tgt == atras){
+        if(contador > 0){
+            img.src = imagenes[contador - 1];
+            contador--;
+                }else{
+                        img.src = imagenes[imagenes.length - 1];
+                        contador = imagenes.length -1;
                         }
-
-                    }else if(tgt == adelante){
-
-                        if(contador < imagenes.length -1){
-                            img.src = imagenes[contador + 1];
-                            contador++;
-                        }else{
-                            img.src = imagenes[0];
-                            contador = 0;
-                        }
+                        }else if(tgt == adelante){
+                            if(contador < imagenes.length -1){
+                                img.src = imagenes[contador + 1];
+                                contador++;
+                            }else{
+                                img.src = imagenes[0];
+                                contador = 0;
+                                }
                     }
                 
+    img.src = imagenes[contador];
+        img.onclick = function(){
+            window.location.href = linksImg[contador];
+                };
+
+
             });
-
-
-    }
+}       
 
 
 document.addEventListener("DOMContentLoaded", () => {
